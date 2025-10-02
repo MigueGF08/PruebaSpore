cf3fb2fab140e81471eb347ea1843c5e6f5a0aa1
Miguel Gonzalez <gmigue653@gmail.com>
2025-10-02 11:33:45 -0600

    Cambio en config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
 
 const common = {
-  dialect: process.env.DB_DIALECT || 'postgres',
-  host: process.env.DB_HOST || '127.0.0.1',
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
   logging: false,
 };
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME || 'postgres',
-    password: process.env.DB_PASSWORD || null,
-    database: process.env.DB_DATABASE || 'database_development',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     ...common,
   }
 };
